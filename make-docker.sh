43f869fa7881a7f611ce4e3ef75f11e57ff7ee4a
Christian Thomas <christian@ckthomas.de>
2020-12-23 19:04:24 +0100

    about to get the image working (actually 	Caused by: java.lang.NoClassDefFoundError: javax/transaction/TransactionManager,... )

--- make-docker.sh
+++ make-docker.sh
@@ -3,19 +3,19 @@
 echo "Clean up target/camunda-docker..."
 rm -rf target/camunda-docker
 mkdir target/camunda-docker
-mkdir target/camunda-docker/config
+#mkdir target/camunda-docker/config
 
-echo "Build camunda-homeassist-automation...
+echo "Build camunda-homeassist-automation..."
 mvn install -f ./pom.xml
 
 echo "Copy Dockerfile to target/camunda-docker..."
 cp ./src/docker/Dockerfile target/camunda-docker/
 
-echo "Copy Configurations to target/camunda-docker/config..."
-cp ./src/docker/config/* target/camunda-docker/config/
+#echo "Copy Configurations to target/camunda-docker/config..."
+#cp ./src/docker/config/* target/camunda-docker/config/
 
-echo "Copy camunda-homeassist-automation to target/camunda-docker..."
-cp target/camunda-homeassist-plugin-1.0.0-SNAPSHOT.jar target/camunda-docker/
+#echo "Copy camunda-homeassist-automation to target/camunda-docker..."
+#cp target/camunda-homeassist-plugin-1.0.0-SNAPSHOT.jar target/camunda-docker/
 
 echo "Build Docker image"
 docker build -f ./target/camunda-docker/Dockerfile -t camunda-homeassist-platform .
@@ -23,4 +23,4 @@ docker build -f ./target/camunda-docker/Dockerfile -t camunda-homeassist-platfor
 echo "Export generated Docker image to to target..."
 docker save camunda-homeassist-platform:latest > target/camunda-docker/camunda-homeassist-platform.tar
 
-echo "Building docker image *camunda-homeassist-platform* finished. Go to target/camunda-docker to find it..."
+echo "Building docker image camunda-homeassist-platform finished :-)"
